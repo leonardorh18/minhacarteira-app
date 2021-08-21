f6ff881ea18cffc8a2a8a0a1bfab5922f68fcb5f
Leonardo Rocha <leoheiro@hotmail.com>
2021-08-21 12:32:30 -0300

    primeiro acesso a API de crypto

--- carteira/lib/views/mercado.dart
+++ carteira/lib/views/mercado.dart
@@ -15,42 +15,153 @@ class Mercado extends StatefulWidget {
   _MercadoState createState() => _MercadoState();
 }
 class _MercadoState extends State<Mercado> {
-  int preco = 0;
-  Future<void> reqp() async {
-  // This example uses the Google Books API to search for books about http.
-  // https://developers.google.com/books/docs/overview
-  var url = Uri.parse('https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false');
+  double preco = 0.0;
+  var moeda = TextEditingController();
+  var crypto = TextEditingController();
+
+  Future<void> reqp(String crypto, String moeda) async {
+
+  if (moeda.trim().isEmpty){
+
+    moeda = 'brl';
+
+  }
+  // url coin 'https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false'
+  var url = Uri.parse('https://api.coingecko.com/api/v3/coins/'+crypto.toLowerCase()+'?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false');
   
   // Await the http get response, then decode the json-formatted response.
   var response = await http.get(url);
   if (response.statusCode == 200) {
     var jsonResponse = convert.jsonDecode(response.body);
     setState(() {
-      preco = jsonResponse['market_data']['current_price']['brl'];
+      preco = jsonResponse['market_data']['current_price'][moeda.toLowerCase()].toDouble();
     });
     
   } else {
     setState(() {
-      preco = 8;
+      preco = 8.0;
     });
   }
   
 }
   @override
 
-  
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-                  GestureDetector(
-                 child: Text("Atualizar", 
-                        style: TextStyle(color: Colors.blue, fontSize: 20),
-                  ),
-                  onTap: reqp,
-                  ),
-            Text(' '+ preco.toString()),
-        ],
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Center(
+                      child: Text('Qual crypto você quer?', 
+                      style: TextStyle(
+                        color: Colors.blueAccent, fontSize: 23, fontWeight: FontWeight.bold),
+                      )
+                    ) ,
+              ),
+                Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Center(
+                      child: Text('*digite o nome da cryptomoeda abaixo*', 
+                      style: TextStyle(
+                        color: Colors.redAccent, fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ) ,
+              ),
+
+                Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Container( 
+                    width: MediaQuery.of(context).size.width / 2 ,
+                    child:TextField(
+                      controller: crypto,
+                    obscureText: false,
+                    maxLength: 100,
+                    decoration: InputDecoration(
+                      
+                      filled: true,
+                      fillColor: Colors.white,
+                        labelText: "Crypto",
+                        contentPadding: EdgeInsets.fromLTRB(5.0, 15.0, 20.0, 10.0),
+                        hintText: "Nome" ,
+                        border:
+                        OutlineInputBorder(borderRadius: BorderRadius.horizontal()),
+                        
+
+                    ),
+                 ),
+                ), 
+              ),
+               Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Center(
+                      child: Text('Moeda', 
+                      style: TextStyle(
+                        color: Colors.blueAccent, fontSize: 23, fontWeight: FontWeight.bold),
+                      )
+                    ) ,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Center(
+                      child: Text('*moeda padrão é BRL*', 
+                      style: TextStyle(
+                        color: Colors.redAccent, fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ) ,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:Container( 
+                    width: MediaQuery.of(context).size.width / 2 ,
+                    child:TextField(
+                    controller: moeda,
+                    obscureText: false,
+                    maxLength: 100,
+                    decoration: InputDecoration(
+                      
+                      filled: true,
+                      fillColor: Colors.white,
+                        labelText: "Moeda",
+                        contentPadding: EdgeInsets.fromLTRB(5.0, 15.0, 20.0, 10.0),
+                        hintText: "Nome ex: brl, usd..." ,
+                        border:
+                        OutlineInputBorder(borderRadius: BorderRadius.horizontal()),
+                        
+
+                    ),
+                 ),
+                ), 
+              ),
+
+              Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.greenAccent[700],
+                    child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width/2,
+                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                      onPressed: () async{
+
+                        await reqp(crypto.text, moeda.text);
+
+                      },
+                      child: Text("Procurar",
+                          textAlign: TextAlign.center,
+
+                    ),
+                    ),
+                ),
+              
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Center(
+                child: Text(moeda.text.toUpperCase()+": "+preco.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                )
+              )
+
+        ]
       ),
     );
   }
